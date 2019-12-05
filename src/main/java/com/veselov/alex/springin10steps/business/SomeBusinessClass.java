05f1bcd950f8eea58dfb07fbe932b2ef0bd0d807
Pogorelov Roman <roman.pogorelov@axamit.com>
2019-12-05 13:18:53 +0300

    Playing with different types of Component.

---
+++ src/main/java/com/veselov/alex/springin10steps/business/SomeBusinessClass.java
@@ -0,0 +1,7 @@
+package com.veselov.alex.springin10steps.business;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SomeBusinessClass {
+}
---
+++ src/main/java/com/veselov/alex/springin10steps/data/UserRepository.java
@@ -0,0 +1,7 @@
+package com.veselov.alex.springin10steps.data;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository {
+}

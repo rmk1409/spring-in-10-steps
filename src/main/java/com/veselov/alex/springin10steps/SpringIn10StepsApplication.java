bdcf8e64c196af0fd993e682aa00dcf2d21f7ab1
Pogorelov Roman <roman.pogorelov@axamit.com>
2019-12-05 10:11:00 +0300

    Add Bean Scope playing.

--- src/main/java/com/veselov/alex/springin10steps/SpringIn10StepsApplication.java
+++ src/main/java/com/veselov/alex/springin10steps/SpringIn10StepsApplication.java
@@ -11,6 +11,9 @@ public class SpringIn10StepsApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(SpringIn10StepsApplication.class, args);
         BinarySearchImp search = context.getBean(BinarySearchImp.class);
+        BinarySearchImp search2 = context.getBean(BinarySearchImp.class);
+		System.out.println(search);
+		System.out.println(search2);
         search.binarySearch(new int[]{1, 10, 100}, 10);
     }
 
--- src/main/java/com/veselov/alex/springin10steps/business/BinarySearchImp.java
+++ src/main/java/com/veselov/alex/springin10steps/business/BinarySearchImp.java
@@ -3,8 +3,11 @@ package com.veselov.alex.springin10steps.business;
 import com.veselov.alex.springin10steps.data.SortAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+@Scope((ConfigurableBeanFactory.SCOPE_PROTOTYPE))
 @Component
 public class BinarySearchImp {
     @Qualifier("quickSort")

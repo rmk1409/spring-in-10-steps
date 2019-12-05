612f113679b1ae8c06c744457e8c93401b439554
Roman Pogorelov <rmk140987@gmail.com>
2019-12-05 06:39:06 +0300

    Initial commit

---
+++ src/main/java/com/veselov/alex/springin10steps/SpringIn10StepsApplication.java
@@ -0,0 +1,13 @@
+package com.veselov.alex.springin10steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIn10StepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIn10StepsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/veselov/alex/springin10steps/SpringIn10StepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veselov.alex.springin10steps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIn10StepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

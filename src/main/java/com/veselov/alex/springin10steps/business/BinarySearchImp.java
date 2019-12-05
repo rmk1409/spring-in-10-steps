e8eaaae240ca5a419c66c86ec365c9761cf6fbd3
Pogorelov Roman <roman.pogorelov@axamit.com>
2019-12-05 11:13:23 +0300

    Playing with bean lifecycle, @PostConstruct / @PreDestroy

--- src/main/java/com/veselov/alex/springin10steps/business/BinarySearchImp.java
+++ src/main/java/com/veselov/alex/springin10steps/business/BinarySearchImp.java
@@ -1,15 +1,22 @@
 package com.veselov.alex.springin10steps.business;
 
 import com.veselov.alex.springin10steps.data.SortAlgorithm;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 @Component
 public class BinarySearchImp {
+    private static Logger LOGGER = LoggerFactory.getLogger(BinarySearchImp.class);
+
     @Qualifier("quickSort")
     @Autowired
     public SortAlgorithm algo;
@@ -18,5 +25,15 @@ public class BinarySearchImp {
         System.out.println(algo.getClass().getName());
         this.algo.sort(ar);
         return 3;
+    }
+
+    @PostConstruct
+    public void postConstruct(){
+        LOGGER.info("postConstruct");
+    }
+
+    @PreDestroy
+    public void preDestroy(){
+        LOGGER.info("PreDestroy");
     }
 }
